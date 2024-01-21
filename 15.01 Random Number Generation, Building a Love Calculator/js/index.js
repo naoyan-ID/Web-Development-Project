2dddce3158ee9a2fb0ff3c0572061a11db0252d6
hdyn29 <fikrisadegha29@gmail.com>
2024-01-21 14:22:49 +0700

    menambahkan folder(13)

---
+++ 15.01 Random Number Generation, Building a Love Calculator/js/index.js
@@ -0,0 +1,13 @@
+var nama = prompt("Masukkan namamu");
+var pasangan = prompt("Masukkan nama pasangan Anda");
+var n = Math.random() * 100;
+n = Math.floor(n) + 1;
+console.log(
+  "hasil kalkulator cinta untuk nama " +
+    nama +
+    " dengan " +
+    pasangan +
+    " adalah " +
+    n +
+    "%"
+);
