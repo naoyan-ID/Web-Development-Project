fffbd94fae37feec0427ef538fe394e887d27cc3
hdyn29 <fikrisadegha29@gmail.com>
2024-04-17 20:23:34 +0700

    add commit

---
+++ 07 Node.js/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+/*
+import sillyName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
+*/
+
+import superheroes from "superheroes";
+const randomHero = superheroes.random();
+console.log(`Im ${randomHero}.`);
